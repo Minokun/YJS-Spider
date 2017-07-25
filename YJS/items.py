ba94d12190e4cd1a8678b1bec2123720e0b82343
Wu XiaoKun <952718180@qq.com>
2017-07-25 13:55:36 +0800

    添加忽略文件

--- YJS/items.py
+++ YJS/items.py
@@ -9,17 +9,21 @@ import scrapy
 from YJS.helper import md5
 import re
 from scrapy.loader import ItemLoader
-from scrapy.loader.processors import TakeFirst, MapCompose
+from scrapy.loader.processors import TakeFirst, MapCompose , Compose
 
 
 def GetMd5(value):
     return md5(value)
 
 
+# 他妹的，这人数居然还有"多人"这个写法！
 def GetRecruitNum(value):
     num_obj = re.match("([\d]*).*", value)
-    num = num_obj.group(1) if num_obj else 0
-    return num
+    if num_obj:
+        return str(num_obj.group(1))
+    else:
+        num_obj = re.match("([\u4e00-\u9fa5]*).*",value)
+        return num_obj.group(1)
 
 
 def GetPositionType(value):
@@ -28,7 +32,11 @@ def GetPositionType(value):
     return position_type
 
 
-class YjsItem(ItemLoader):
+def RemoveBlankCharacter(value):
+    return value.replace("\r\n","").replace("\t","")
+
+
+class YjsItemLoader(ItemLoader):
     # 自定义itemloader
     default_output_processor = TakeFirst()
 
@@ -53,10 +61,14 @@ class YjsSelfItem(scrapy.Item):
     position_type = scrapy.Field(
         input_processor = MapCompose(GetPositionType),
     )
-    position_desc = scrapy.Field()
-    company_intro = scrapy.Field()
+    position_desc = scrapy.Field(
+        input_processor=MapCompose(RemoveBlankCharacter),
+    )
+    company_intro = scrapy.Field(
+        input_processor=MapCompose(RemoveBlankCharacter),
+    )
     company_site = scrapy.Field()
-    create_at = scrapy.Field()
+    created_at = scrapy.Field()
     valid_date = scrapy.Field()
 
 
@@ -74,5 +86,7 @@ class YjsOtherItem(scrapy.Item):
     position_type = scrapy.Field()
     source = scrapy.Field()
     major_label = scrapy.Field()
-    content = scrapy.Field()
+    content = scrapy.Field(
+        input_processor = MapCompose(RemoveBlankCharacter),
+    )
     created_at = scrapy.Field()
--- YJS/pipelines.py
+++ YJS/pipelines.py
@@ -18,7 +18,7 @@ class YjsPipeline(object):
     def from_settings(cls,settings):
         dbparms = dict(
             host = settings['MYSQL_HOST'],
-            user = settings['MYsql_USER'],
+            user = settings['MYSQL_USER'],
             password = settings['MYSQL_PASSWORD'],
             db = settings['MYSQL_DBNAME'],
             charset = 'utf8',
@@ -33,15 +33,103 @@ class YjsPipeline(object):
         # 异步插入
         query = self.dbpool.runInteraction(self.do_insert,item)
         # 处理异常
-        query.addErrback(self.handle_error)
+        query.addErrback(self.handle_error, item, spider)
 
     def handle_error(self,failure,item,spider):
         print(failure)
+        import codecs
+        fw = codecs.open('errLog.txt', 'a', 'utf-8')
+        fw.write("网站地址：" + item['url'] + "\r\n" + str(failure))
+        fw.close()
+
+    def KeyJudge(self, item_dict, value):
+        if value in item_dict.keys():
+            return item_dict[value]
+        else:
+            return None
 
     def do_insert(self,cursor,item):
-        insert_sql = """
-            insert into table(cols1,cols2)
-            VALUES (%s,%s)
-        """
-        cursor.execute(insert_sql,(item[''],item['']))
+
+        # 判断是哪种类型 1,本站 0 其他网站
+        if item['type'] == 0:
+            # 如果连公司名都获取不到，那就是直接跳转到官网去了
+            if self.KeyJudge(item, "company"):
+                location = self.KeyJudge(item, 'location')
+                major_label = self.KeyJudge(item, 'major_label')
+
+                insert_sql = """
+                    insert into yjs_other(
+                    url,
+                    url_md5,
+                    title,
+                    tag,
+                    company,
+                    post_date,
+                    location,
+                    position_type,
+                    source,
+                    major_label,
+                    content,
+                    created_at)
+                    VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+                """
+                cursor.execute(insert_sql, (
+                    item['url'],
+                    item['url_md5'],
+                    item['title'],
+                    item['tag'],
+                    item['company'],
+                    item['post_date'],
+                    location,
+                    item['position_type'],
+                    item['source'],
+                    major_label,
+                    item['content'],
+                    item['created_at']
+                ))
+        else:
+
+            recruit_num = self.KeyJudge(item, 'recruit_num')
+
+            insert_sql = """
+                insert into yjs_self(
+                url,
+                url_md5,
+                title,
+                tag,
+                company,
+                industry,
+                company_size,
+                company_type,
+                position_title,
+                location,
+                recruit_num,
+                position_type,
+                position_desc,
+                company_intro,
+                company_site,
+                created_at,
+                valid_date)
+                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+            """
+            cursor.execute(insert_sql, (
+                item['url'],
+                item['url_md5'],
+                item['title'],
+                item['tag'],
+                item['company'],
+                item['industry'],
+                item['company_size'],
+                item['company_type'],
+                item['position_title'],
+                item['location'],
+                recruit_num,
+                item['position_type'],
+                item['position_desc'],
+                item['company_intro'],
+                item['company_site'],
+                item['created_at'],
+                item['valid_date']
+            ))
+
 
